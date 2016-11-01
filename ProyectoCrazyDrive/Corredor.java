70715fe98283d7694323b55f8b6887072a87297b
MarycruzBustos <bhmary25@gmail.com>
2016-10-31 20:53:27 -0600

    Creación de Clases
    Se creo las clases del corredor principal y el enemigo

---
+++ ProyectoCrazyDrive/Corredor.java
@@ -0,0 +1,18 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Corredor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Corredor extends Actor
+{
+    public Corredor(){
+    }
+    
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ ProyectoCrazyDrive/CrazyDriveWorld.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CrazyDriveWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CrazyDriveWorld extends World
+{
+public static final int WIDTH = 500;
+    public static final int HEIGHT = 550;  
+    /**
+     * Constructor for objects of class CrazyDriveWorld.
+     * 
+     */
+    public CrazyDriveWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super( WIDTH, HEIGHT, 1); 
+    }
+}
